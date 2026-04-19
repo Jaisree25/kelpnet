a1870e8d10ee0f51c503a16204a0d18e22d197c1
Swarna Kona <skn.kona@gmail.com>
2026-04-19 14:01:50 -0700

    Updated: flutter connect to web

--- web-dashboard/web/firebase-config.js
+++
@@ -1,9 +0,0 @@
-const GOOGLE_MAPS_KEY = "AIzaSyCac6aTe_e02E9cmho4oq_mmhdLW8XzMaU";
-
-const script = document.createElement("script");
-script.src =
-  `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_KEY}&libraries=marker&v=weekly&callback=initMap`;
-script.async = true;
-script.defer = true;
-
-document.head.appendChild(script);
